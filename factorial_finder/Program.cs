fe96cb20e12c0b7a60d2eec29621de4980155b4d
Cormac Reilly <16ReillC@fulford.york.sch.uk>
2022-01-05 10:00:17 +0000

    Added a method and if statments

--- factorial_finder/Program.cs
+++ factorial_finder/Program.cs
@@ -10,7 +10,32 @@ namespace factorial_finder
     {
         static void Main(string[] args)
         {
+            //This is the problem The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. 
+            // https://live.withcode.uk/alevel/?id=1 Solve this using both loops and recursion
             Console.WriteLine("welcome to factorial finder");
+            int inp = Int32.Parse(Console.ReadLine());
+
+            int factorial(int input)
+            {
+
+                int x = input;
+                if (input == 0)
+                {
+                    return 1;
+                }
+
+
+                if (input > 0)
+                {
+
+                }
+
+
+            }
+
+            Console.WriteLine(factorial(inp));
+
+
         }
     }
 }
