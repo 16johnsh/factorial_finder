e745eaebab691c2886e67024d8560c4a78363977
Henry Johnson <16JohnsH@fulford.york.sch.uk>
2022-01-10 13:06:30 +0000

    Added console ReadLine and ask user for input

--- factorial_finder/Program.cs
+++ factorial_finder/Program.cs
@@ -12,33 +12,26 @@ namespace factorial_finder
         {
             //This is the problem The Factorial of a positive integer, n, is defined as the product of the sequence n, n-1, n-2, ...1 and the factorial of zero, 0, is defined as being 1. 
             // https://live.withcode.uk/alevel/?id=1 Solve this using both loops and recursion
+
             Console.WriteLine("welcome to factorial finder");
+            Console.Write("please enter a positive integer: ");
             int inp = Int32.Parse(Console.ReadLine());
             int x = 1;
 
-
-            Console.WriteLine(factorial(inp, x));
-
-            
+            Console.WriteLine(factorial(inp, x));            
 
             int factorial(int input, int y)
-            {
-
-                
+            {                
                 if (input == 0)
                 {
                     return y;
                 }
                 y = input * y;
                 input = input - 1;
-                
+
                 return (factorial(input, y));
-
             }
-
-            
-
-
+            Console.ReadLine();
         }
     }
 }
